f0bd15cfca9491d8fc8a9f460334bdedf558c385
Amir Yankolovich <amiryankolovich2000@gmail.com>
2024-10-31 13:42:05 +0200

    Calc the avg in the dice question

--- Random dice with Array from/arrayFrom.js
+++ Random dice with Array from/arrayFrom.js
@@ -2,3 +2,14 @@
 // Each element is a random number between 0 and 6
 const dice = Array.from({ length: 100 }, () => Math.floor(Math.random() * 7))
 console.log(dice)
+
+function calcAvgDiceRoll(diceArray) {
+  const sum = diceArray.reduce((acc, num) => acc + num, 0)
+  const avg = Math.trunc(sum / diceArray.length)
+  return avg
+}
+const avg = calcAvgDiceRoll(dice)
+console.log('avg:', avg)
+
+
+
