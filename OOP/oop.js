73c46bc34d3616920b4d114d40cbb141bf18d498
Amir Yankolovich <amiryankolovich2000@gmail.com>
2024-11-10 12:34:42 +0200

    Prototype example

--- OOP/oop.js
+++ OOP/oop.js
@@ -8,3 +8,22 @@ Array.prototype.printItems = function () {
 const numbers = [1, 2, 3]
 
 numbers.printItems()
+
+// Constructor Function
+
+function User(name, pass) {
+  this.name = name
+  this.pass = pass
+}
+
+
+//Adding a prototype to the User Constructor
+User.prototype.checkIfPass = function (pass) {
+  return pass === this.pass
+}
+
+const user1 = new User('bobi', 'secret')
+console.log('user1:', user1)
+
+const isPass = user1.checkIfPass('secret')
+console.log('result:', isPass)
