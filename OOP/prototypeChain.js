a6b1a91a12dcb6151c1a4ba9d5c8477e79ec637b
Amir Yankolovich <amiryankolovich2000@gmail.com>
2024-11-11 11:34:03 +0200

    Prototype chain example

---
+++ OOP/prototypeChain.js
@@ -0,0 +1,7 @@
+// The console.dir(h1) command provides a more detailed look into the properties of the h1 element,
+//  including the prototype chain, which lists all inherited
+//  properties and methods that are part of the HTMLElement
+//  prototype and beyond.
+const h1 = document.querySelector('h1')
+console.log(h1)
+console.dir(h1)
