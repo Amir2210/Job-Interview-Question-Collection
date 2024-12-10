ac2bc2d055460acf51cf1688bc91b0271450f557
Amir Yankolovich <amiryankolovich2000@gmail.com>
2024-12-10 11:55:08 +0200

    Add the basic how to write promise in js

--- promise/promise.js
+++ promise/promise.js
@@ -1,7 +1,31 @@
-const myPromise = new Promise((res, rej) => {
+const myPromiseSuccess = new Promise((res, rej) => {
   res('promise fulfilled')
 })
 
-myPromise.then((result) => {
+myPromiseSuccess.then((result) => {
   console.log(result)
+})
+
+
+const myPromiseFail = new Promise((res, rej) => {
+  rej('Promise Rejected')
+})
+
+myPromiseFail.then((result) => {
+  console.log('Promise Success')
+}).catch((result) => {
+  console.log(result)
+})
+
+const chainPromise = new Promise((res, rej) => {
+  2 > 1 ? res(2) : rej('ERROR!')
+})
+
+chainPromise.then((result) => {
+  console.log(result)
+  return result
+}).then((result) => {
+  console.log(`result + 1: ${result + 1}`)
+}).catch((err) => {
+  console.log(err)
 })
