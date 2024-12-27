0b658012d97be63626b7b8986abdd87be86051e6
Amir Yankolovich <amiryankolovich2000@gmail.com>
2024-12-27 15:57:28 +0200

    Find node in tree ex

---
+++ trees/findNode.js
@@ -0,0 +1,67 @@
+const tree = {
+  id: 1,
+  name: "Root",
+  children: [
+    {
+      id: 2,
+      name: "Child 1",
+      children: [
+        {
+          id: 4,
+          name: "Grandchild 1",
+          children: [{ id: 5, name: 'GrandGrandChild 1', children: [] }],
+        },
+      ],
+    },
+    {
+      id: 3,
+      name: "Child 2",
+      children: [{ id: 6, name: 'Grandchild 2', children: [] }],
+    },
+  ],
+}
+
+// DFS: מתאים למצבים שבהם חשוב להעמיק ולבדוק ענף אחד עד הסוף.
+function findNode(tree, trackId) {
+  if (tree.id === trackId) {
+    return tree
+  }
+
+  for (const child of tree.children) {
+    const result = findNode(child, trackId)
+    if (result) {
+      return result
+    }
+  }
+  return null
+}
+
+
+const result = findNode(tree, 4)
+// console.log(result)
+
+
+
+//כל פעם בודקים אם לצומת יש ילדים אם כן נבדוק אותם גם וכן הלאה
+function findNodeBFS(tree, trackId) {
+  const queue = [tree]
+
+  while (queue.length > 0) {
+    const current = queue.shift()
+
+    if (current.id === trackId) {
+      return current
+    }
+
+    queue.push(...current.children)
+  }
+
+  return null
+}
+
+// בדיקה של הפונקציה
+const resultBFS = findNodeBFS(tree, 5);
+console.log(resultBFS)
+
+
+
